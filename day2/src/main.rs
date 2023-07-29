d7d3880e9763611ef1cdb3543aa21df534b87f83
HellCat46 <hellcat2388@gmail.com>
2023-07-29 12:45:31 +0530

    Combine both parts of day 2 in one folder

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,10 +5,9 @@ struct RPS {
 fn main() {
     let data = std::fs::read_to_string("data/rps.txt").expect("Can't find file");
     let rounds = data.split("\n");
-
+  
     let mut me = RPS { score : 0 };
-
-    for round in rounds {
+    for round in rounds.clone() {
         match round {
             "A X" => me.draw(1),
             "A Y" => me.won(2),
@@ -23,7 +22,32 @@ fn main() {
         }
     }
 
-    println!("Score: {}", me.score);
+    println!("Part 1: {}", me.score);
+
+    
+
+    let possibilities = [
+        "B X", "C X", "A X",
+        "A Y", "B Y", "C Y",
+        "C Z", "A Z", "B Z"
+    ];
+    let mut points = 0;
+
+    for round in rounds {
+        
+        let outcome = match  possibilities.iter().position(|&poss| poss == round) {
+            Some(postion) => postion,
+            None => panic!("Something is wrong with input")
+        };
+        
+        if outcome >= 3 && outcome <= 5{
+            points += 3;
+        }else if outcome >= 6 && outcome <= 8  {
+            points += 6;
+        }
+        points += (outcome % 3) + 1;
+    }
+    println!("Part 2: {}", points);
 }
 
 impl RPS {
