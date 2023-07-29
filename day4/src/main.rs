f0a4725d109cd1ec21ffb09a262e8c638ca0a1f3
HellCat46 <hellcat2388@gmail.com>
2023-07-29 15:56:09 +0530

    Solved p2 of day 4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -5,22 +5,33 @@ fn main() {
 
 
     let mut count = 0;
-    for pair in data.split("\n") {
+    let mut p2_count = 0;
+    for pairs in data.split("\n") {
 
-        let elfs : Vec<_>= pair.split(",").collect::<Vec<&str>>().iter().map(|i| i.split("-")).collect();
+        let elfs : Vec<_>= pairs.split(",").collect::<Vec<&str>>().iter().map(|i| i.split("-")).collect();
         
-        let mut a : Vec<(u32,u32)> = Vec::new();
+        let mut pair : Vec<(u32,u32)> = Vec::new();
         for mut elf in elfs {
-                a.push((elf.nth(0).unwrap().trim().parse().unwrap(), elf.nth(0).unwrap().trim().parse().unwrap()));
+                pair.push((elf.nth(0).unwrap().trim().parse().unwrap(), elf.nth(0).unwrap().trim().parse().unwrap()));
         }
 
-        let cmp_first = a[0].0  >= a[1].0 && a[0].1 <= a[1].1;
-        let cmp_sec = a[1].0  >= a[0].0 && a[1].1 <= a[0].1;
-  
+        let cmp_first = pair[0].0  >= pair[1].0 && pair[0].1 <= pair[1].1;
+        let cmp_sec = pair[1].0  >= pair[0].0 && pair[1].1 <= pair[0].1;
+        
         if cmp_first || cmp_sec {
             count +=1;
         }
+
+        // Part 2
+
+        if pair[0].0 <= pair[1].1 && pair[1].0 <= pair[0].1{
+            p2_count += 1;
+        }
+
+        
     }
 
-    println!("{count}")
+    println!("Part 1: {count} \nPart 2: {p2_count}")
 }
+
+
